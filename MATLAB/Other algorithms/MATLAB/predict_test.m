869f5df0a6e1df1198534e5f783c0d85f3f1f44e
Adithya Ganesh <acganesh@stanford.edu>
2013-09-15 19:52:43 -0500

    Deleted messy MATLAB directory

--- MATLAB/Other algorithms/MATLAB/predict_test.m
+++
@@ -1,6 +0,0 @@
-wb_test = predict(linclass, waterbottle_m)
-k_test = predict(linclass, keys_m)
-w_test = predict(linclass, wallet_m)
-tb_test = predict(linclass, toothbrush_m)
-us_test = predict(linclass, unscrewing_m)
-
